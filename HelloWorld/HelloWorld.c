cc00cedf27812afe9104feaf837d62ab8284c0f5
Alexander <pchp250g8@tutamail.com>
2026-02-04 11:49:50 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld/HelloWorld.c
@@ -0,0 +1,90 @@
+﻿// HelloWorld.c : Определяет точку входа для приложения.
+//
+#include "framework.h"
+#include "HelloWorld.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;// текущий экземпляр
+
+// Отправить объявления функций, включенных в этот модуль кода:
+BOOL                InitInstance(HINSTANCE, int);
+INT_PTR CALLBACK    HelloWorldDlg(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    return 0;
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+    INT_PTR nResponse = DialogBox(hInst, MAKEINTRESOURCE(IDD_HELLOWORLD_DIALOG), NULL, HelloWorldDlg);
+    if (nResponse == IDOK)
+    {
+        // TODO: Введите код для обработки закрытия диалогового окна
+        //  с помощью кнопки "ОК"
+    }
+    else if (nResponse == IDCANCEL)
+    {
+        // TODO: Введите код для обработки закрытия диалогового окна
+        //  с помощью кнопки "Отмена"
+    }
+    else if (nResponse == -1)
+        return FALSE;
+   return TRUE;
+}
+
+// Обработчик сообщений для окна "HelloWorld".
+INT_PTR CALLBACK HelloWorldDlg(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        if (LOWORD(wParam) == IDC_BUTTON1)
+        {
+            MessageBox(hDlg, _T("Hello,World!!!"), _T("Hello"), 0);
+        }
+        if (LOWORD(wParam) == IDC_BUTTON2)
+        {
+            EndDialog(hDlg, 0);
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ HelloWorld/resource.h
@@ -0,0 +1,24 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется HelloWorld.rc
+//
+#define IDC_MYICON                      2
+#define IDD_HELLOWORLD_DIALOG           102
+#define IDI_HELLOWORLD                  107
+#define IDI_SMALL                       108
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
